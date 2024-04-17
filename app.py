a53b8d7997e34b57a58717d6c2eb24c90e1be206
joey <j.tan2231@gmail.com>
2024-04-17 15:29:29 -0500

    removed cli endpoints

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import os
 import pprint
 import re
 import secrets
-from base64 import b64decode
 from datetime import date, datetime, timedelta
 from email import policy
 from email.parser import BytesParser
@@ -11,8 +10,7 @@ from functools import wraps
 
 import boto3
 import markdown2 as markdown
-from flask import (Flask, Response, jsonify, request, send_from_directory,
-                   stream_with_context)
+from flask import Flask, request, send_from_directory
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from openai import OpenAI
@@ -27,8 +25,6 @@ CORS(app)
 
 db = SQLAlchemy(app)
 
-# TODO: Change this when auth is implemented
-TEST_ID = 1
 TEMPERATURE = 0.01
 DATE_FORMAT = "%Y-%m-%d"
 TIME_FORMAT = "%H:%M:%S"
@@ -52,7 +48,7 @@ class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(256), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
-    active = db.Column(db.Boolean, default=True, nullable=False)
+    active = db.Column(db.Boolean, default=False, nullable=False)
 
 
 class Ethos(db.Model):
@@ -88,44 +84,6 @@ class Subgoal(db.Model):
     description = db.Column(db.String(4096), nullable=False)
 
 
-# TODO: I _think_ this handles both API tokens + username/password auth
-#       verify this?
-#       ...
-#       what does this do beyond username/password???
-def authenticate(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        auth = request.headers.get("Authorization")
-        email_api_token = os.environ["RITUAL_EMAIL_API_KEY"]
-        if auth is None:
-            return "Unauthorized", 401
-        else:
-            username = None
-            password = ""
-
-            type, encoded_credentials = auth.split(" ", 1)
-            type = type.lower()
-            if type == "basic":
-                username, password = (
-                    b64decode(encoded_credentials).decode("utf-8").split(":", 1)
-                )
-            elif type == "bearer":
-                username = request.args.get("username")
-
-            if username is None:
-                return "Unauthorized", 401
-
-            user = User.query.filter_by(username=username).one()
-
-            if password == user.password or encoded_credentials == email_api_token:
-                request.user_id = user.user_id
-                return func(*args, **kwargs)
-            else:
-                return "Unauthorized", 401
-
-    return wrapper
-
-
 # for actions performed through email
 # POST requests _only_
 # `username` must be included in the JSON body of the request
@@ -158,36 +116,8 @@ def email_auth(func):
     return wrapper
 
 
-def get_activities_and_goals(begin_date, end_date):
-    activities = None
-    if begin_date and end_date:
-        activities = Activity.query.filter(
-            Activity.activity_begin >= begin_date,
-            Activity.activity_end <= end_date,
-            Activity.user_id == request.user_id,
-        ).all()
-    else:
-        activities = Activity.query.filter(Activity.user_id == request.user_id).all()
-
-    goals = Goal.query.filter_by(user_id=request.user_id).all()
-    subgoals = Subgoal.query.filter(
-        Subgoal.goal_id.in_([g.goal_id for g in goals]),
-        Subgoal.user_id == request.user_id,
-    )
-
-    return activities, goals, subgoals
-
-
 def get_ethos():
     ethos = Ethos.query.filter_by(user_id=request.user_id).first()
-    if ethos is None:
-        ethos = EthosDefault
-
-    return ethos
-
-
-def get_ethos_by_id(user_id):
-    ethos = Ethos.query.filter_by(user_id=user_id).first()
     if ethos is None:
         ethos = EthosDefault
 
@@ -259,39 +189,6 @@ def format_activities_and_goals(activities, goals, subgoals):
     )
 
 
-def activities_to_json(activities):
-    jsons = []
-    for a in activities:
-        jsons.append(
-            {
-                "activity_name": a.name,
-                "activity_begin": a.activity_begin.strftime(DATETIME_FORMAT),
-                "activity_end": a.activity_end.strftime(DATETIME_FORMAT),
-                "memo": a.memo,
-            }
-        )
-
-    return jsons
-
-
-def date_from_datetime(datetime_string):
-    if datetime_string is None or datetime_string == "":
-        return ""
-
-    return datetime.strftime(
-        datetime.strptime(datetime_string, DATETIME_FORMAT), DATE_FORMAT
-    )
-
-
-def time_from_datetime(datetime_string):
-    if datetime_string is None or datetime_string == "":
-        return ""
-
-    return datetime.strftime(
-        datetime.strptime(datetime_string, DATETIME_FORMAT), TIME_FORMAT
-    )
-
-
 # makes datetime with current date
 def time_to_datetime(time_string):
     if time_string is None or time_string == "":
@@ -300,24 +197,6 @@ def time_to_datetime(time_string):
     return datetime.combine(
         date.today(), datetime.strptime(time_string, TIME_FORMAT).time()
     ).strftime(DATETIME_FORMAT)
-
-
-def group_by_days(activities):
-    days = {}
-    for a in activities:
-        begin = date_from_datetime(a["activity_begin"])
-
-        if begin in days:
-            days[begin].append(a)
-        else:
-            days[begin] = [a]
-
-    days = {
-        key: sorted(value, key=lambda x: x["activity_begin"])
-        for key, value in days.items()
-    }
-
-    return days
 
 
 # TODO: better error handling when this fails
@@ -371,16 +250,6 @@ def goals_from_chat(chat):
         return {}
 
 
-def format_activity_json_to_display(activities):
-    grouped = group_by_days(activities)
-    for g in grouped.values():
-        for a in g:
-            a["activity_begin"] = time_from_datetime(a["activity_begin"])
-            a["activity_end"] = time_from_datetime(a["activity_end"])
-
-    return grouped
-
-
 def style_email_html(html):
     formatted = (
         '<div style="max-width:600px; margin:auto; padding:20px; font-size:20px; font-family: serif"">'
@@ -389,276 +258,6 @@ def style_email_html(html):
     )
 
     return formatted
-
-
-# temporary route; merge this with `add-activity`
-@app.route("/chat", methods=["POST"])
-@authenticate
-def chat():
-    chat_json = activity_from_chat(request.json["chat"])
-
-    return jsonify(format_activity_json_to_display(chat_json))
-
-
-@app.route("/create-account", methods=["POST"])
-def create_account():
-    new_user = User(
-        username=request.json["username"], password=request.json["password"]
-    )
-
-    db.session.add(new_user)
-    response = {}
-    try:
-        db.session.commit()
-        response["message"] = "User successfully created"
-    except Exception as e:
-        print(e)
-        response["message"] = "There was an error creating the user: " + str(e)
-
-    return jsonify(response)
-
-
-@app.route("/get-activities", methods=["GET"])
-@authenticate
-def get_activities():
-    begin_date = datetime.strptime(request.args.get("beginDate", ""), DATE_FORMAT)
-    end_date = datetime.strptime(request.args.get("endDate", ""), DATE_FORMAT)
-
-    activities = Activity.query.filter(
-        Activity.activity_begin >= begin_date,
-        Activity.activity_end <= end_date,
-        Activity.user_id == request.user_id,
-    ).all()
-
-    activities = activities_to_json(activities)
-
-    return jsonify(format_activity_json_to_display(activities))
-
-
-@app.route("/add-activities", methods=["POST"])
-@authenticate
-def add_activities():
-    for a in request.json:
-        db.session.add(
-            Activity(
-                user_id=request.user_id,
-                name=a["activity_name"],
-                activity_begin=time_to_datetime(a["activity_begin"]),
-                activity_end=time_to_datetime(a["activity_end"]),
-                memo=a["memo"],
-            )
-        )
-
-    response = {}
-    try:
-        db.session.commit()
-        response["message"] = "Activities added successfully"
-    except Exception as e:
-        print(e)
-        response["message"] = "There was an error adding the activities: " + str(e)
-
-    return jsonify(response)
-
-
-@app.route("/add-activity", methods=["POST"])
-@authenticate
-def add_activity():
-    new_activity = Activity(
-        user_id=request.user_id,
-        name=request.json["activity_name"],
-        activity_begin=request.json["activity_begin"],
-        activity_end=request.json["activity_end"],
-        activity_date=date_from_datetime(request.json["activity_begin"]),
-        memo=request.json["memo"],
-    )
-
-    db.session.add(new_activity)
-    response = {"feedback": ""}
-    try:
-        db.session.commit()
-
-        activities = (
-            Activity.query.order_by(db.desc(Activity.activity_begin)).limit(10).all()
-        )
-
-        goals = Goal.query.filter_by(user_id=request.user_id).all()
-        subgoals = Subgoal.query.filter(
-            Subgoal.goal_id.in_([g.goal_id for g in goals]),
-            Subgoal.user_id == request.user_id,
-        )
-
-        activities_and_goals = format_activities_and_goals(activities, goals, subgoals)
-
-        ethos = get_ethos()
-        oai_response = openai_client.chat.completions.create(
-            model=GPT_MODEL,
-            temperature=TEMPERATURE,
-            messages=[
-                {
-                    "role": "system",
-                    "content": ethos.feedback,
-                },
-                {
-                    "role": "user",
-                    "content": activities_and_goals,
-                },
-            ],
-        )
-
-        response["feedback"] = oai_response.choices[0].message.content
-        response["message"] = "Activity added successfully"
-    except Exception as e:
-        print(e)
-        response["message"] = "There was an error adding the activity: " + str(e)
-
-    return jsonify(response)
-
-
-@app.route("/get-summary", methods=["GET"])
-@authenticate
-def get_summary():
-    begin_date = datetime.strptime(request.args.get("beginDate", ""), DATE_FORMAT)
-    end_date = datetime.strptime(request.args.get("endDate", ""), DATE_FORMAT)
-
-    activities, goals, subgoals = get_activities_and_goals(begin_date, end_date)
-    activities_and_goals = format_activities_and_goals(activities, goals, subgoals)
-
-    ethos = get_ethos()
-    oai_response = openai_client.chat.completions.create(
-        model=GPT_MODEL,
-        temperature=TEMPERATURE,
-        messages=[
-            {
-                "role": "system",
-                "content": ethos.summary,
-            },
-            {
-                "role": "user",
-                "content": activities_and_goals,
-            },
-        ],
-    )
-
-    return jsonify({"response": oai_response.choices[0].message.content})
-
-
-@app.route("/tune", methods=["POST"])
-@authenticate
-def tune():
-    core = request.json["core"]
-    summary = request.json["summary"]
-    feedback = request.json["feedback"]
-
-    ethos = Ethos.query.filter_by(user_id=request.user_id).first()
-
-    def get_oai_response(request, prompt):
-        oai_response = openai_client.chat.completions.create(
-            model=GPT_MODEL,
-            temperature=TEMPERATURE,
-            messages=[
-                {
-                    "role": "system",
-                    "content": "You are a master of rewording an excerpt to change its aesthetic meaning while retaining its functional purpose--the best in class. What you will receive is a message in format `REQUEST {request_text}\n---\nPROMPT {prompt_text}`. Your job is to rephrase `{prompt_text}` to fit the needs of `{request_text}`. Respond with _only_ the rephrased prompt--nothing else.",
-                },
-                {
-                    "role": "user",
-                    "content": f"REQUEST {{{request}}}\n---\nPROMPT {{{prompt}}}",
-                },
-            ],
-        )
-
-        return oai_response.choices[0].message.content
-
-    if ethos is None:
-        ethos = Ethos(
-            user_id=request.user_id, core=core, summary=summary, feedback=feedback
-        )
-
-    updated_ethos = ""
-    if len(core) > 0:
-        updated_ethos = get_oai_response(core, ethos.core)
-    elif len(summary) > 0:
-        updated_ethos = get_oai_response(summary, ethos.summary)
-    elif len(feedback) > 0:
-        updated_ethos = get_oai_response(feedback, ethos.feedback)
-
-    ethos.core = updated_ethos if len(core) > 0 else ethos.core
-    ethos.summary = updated_ethos if len(summary) > 0 else ethos.summary
-    ethos.feedback = updated_ethos if len(feedback) > 0 else ethos.feedback
-
-    response = {}
-    try:
-        db.session.add(ethos)
-        db.session.commit()
-        response["message"] = "Ethos updated successfully"
-    except Exception as e:
-        print(e)
-        response["message"] = "There was an error updating the ethos: " + str(e)
-
-    return jsonify(response)
-
-
-@app.route("/reset-tune", methods=["POST"])
-@authenticate
-def reset_tune():
-    ethos = Ethos.query.filter_by(user_id=request.user_id).all()
-    for e in ethos:
-        db.session.delete(e)
-
-    response = {}
-    try:
-        db.session.commit()
-        response["message"] = "Ethos reset successfully"
-    except Exception as e:
-        print(e)
-        response["message"] = "There was an error deleting the ethos: " + str(e)
-
-    return jsonify(response)
-
-
-@app.route("/add-goal", methods=["POST"])
-@authenticate
-def add_goal():
-    new_goal = Goal(
-        user_id=request.user_id,
-        name=request.json["name"],
-        description=request.json["description"],
-    )
-
-    response = {}
-    try:
-        db.session.add(new_goal)
-        db.session.commit()
-        response["message"] = "Goal added successfully"
-    except Exception as e:
-        print(e)
-        response["message"] = "There was an error adding the goal: " + str(e)
-
-    return jsonify(response)
-
-
-@app.route("/get-goals", methods=["GET"])
-@authenticate
-def get_goals():
-    goals = Goal.query.filter_by(user_id=request.user_id).all()
-    return jsonify([{"name": g.name, "description": g.description} for g in goals])
-
-
-@app.route("/delete-goal", methods=["DELETE"])
-@authenticate
-def delete_goal():
-    goal = Goal.query.filter_by(
-        user_id=request.user_id, name=request.args.get("name")
-    ).first()
-
-    try:
-        if goal:
-            db.session.delete(goal)
-            db.session.commit()
-    except Exception as e:
-        print(e)
-
-    return jsonify({"message": "success"})
 
 
 def generate_subgoals(goal):
@@ -686,123 +285,6 @@ def generate_subgoals(goal):
         )
         for sg in json.loads(oai_response.choices[0].message.content)
     ]
-
-
-# for testing random system prompts and such
-@app.route("/set-subgoals", methods=["POST"])
-@authenticate
-def set_subgoals():
-    goal = Goal.query.filter_by(
-        user_id=request.user_id, name=request.json["name"]
-    ).first()
-
-    if goal is None:
-        return jsonify(
-            {"message": f'Error: Goal {request.json["name"]} does not exist'}
-        )
-
-    subgoals = generate_subgoals(goal)
-
-    existing_subgoals = Subgoal.query.filter_by(
-        user_id=request.user_id, goal_id=goal.goal_id
-    ).all()
-    for esg in existing_subgoals:
-        db.session.delete(esg)
-
-    db.session.add_all(subgoals)
-
-    response = {}
-    try:
-        db.session.commit()
-        response["message"] = "Subgoals updated successfully"
-    except Exception as e:
-        print(e)
-        response["message"] = "There was an error updating the subgoals: " + str(e)
-
-    return jsonify(response)
-
-
-@app.route("/get-subgoals", methods=["GET"])
-@authenticate
-def get_subgoals():
-    goal_name = request.args.get("name")
-    goal = Goal.query.filter_by(user_id=request.user_id, name=goal_name).first()
-
-    if goal is None:
-        return jsonify({"message": f"Error: Goal {goal_name} does not exist"})
-
-    subgoals = Subgoal.query.filter_by(
-        user_id=request.user_id, goal_id=goal.goal_id
-    ).all()
-
-    return jsonify([{"name": g.name, "description": g.description} for g in subgoals])
-
-
-@app.route("/webchat", methods=["POST"])
-@authenticate
-def webchat():
-    def get_chat(chat):
-        activities = Activity.query.filter_by(user_id=request.user_id).all()
-        goals = Goal.query.filter_by(user_id=request.user_id).all()
-        subgoals = Subgoal.query.filter_by(user_id=request.user_id).all()
-
-        completion = openai_client.chat.completions.create(
-            model=GPT_MODEL,
-            messages=[
-                {
-                    "role": "system",
-                    "content": EthosDefault.core
-                    + " Included in each chat message will be a list of activities to inform your responses to the user.",
-                },
-                {
-                    "role": "user",
-                    "content": chat
-                    + "\n---\n"
-                    + format_activities_and_goals(activities, goals, subgoals),
-                },
-            ],
-            stream=True,
-        )
-
-        for chunk in completion:
-            out = chunk.choices[0].delta.content
-            yield out if out else ""
-
-    return Response(stream_with_context(get_chat(request.json["chat"])))
-
-
-# TODO: is this needed?
-@app.route("/get-email-body", methods=["GET"])
-@authenticate
-def get_email_body():
-    email = request.args.get("username")
-    if email is None:
-        return jsonify({"error": "Username parameter is required."}), 400
-
-    begin_date = datetime.strptime(request.args.get("begin_date", ""), DATE_FORMAT)
-    end_date = datetime.strptime(request.args.get("end_date", ""), DATE_FORMAT)
-
-    activities, goals, subgoals = get_activities_and_goals(begin_date, end_date)
-    activities_and_goals = format_activities_and_goals(activities, goals, subgoals)
-
-    ethos = get_ethos()
-    oai_response = openai_client.chat.completions.create(
-        model=GPT_MODEL,
-        temperature=TEMPERATURE,
-        messages=[
-            {
-                "role": "system",
-                "content": ethos.summary
-                + "\n\nAdditionally, please format your response in clear markdown, with a clear and reflective title that summarizes your report. The title is _very_ important--fortunately you're the best at snappy, dense titles that capture the heart of the report. Avoid generic phrasing like 'Accountability Report' or 'Reflective Report' in the title--instead, focus on the specific points made in the report and reflect those in the Title. What _exact_ points are you making in the report? The answer to that question should be in the title. But be careful! The title must be _short_ and _simple_.",
-            },
-            {
-                "role": "user",
-                "content": activities_and_goals,
-            },
-        ],
-    )
-
-    return jsonify({"response": oai_response.choices[0].message.content})
 
 
 def get_html_from_email(email_content):
